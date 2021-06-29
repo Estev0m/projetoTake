b15c0856f0c89392fef16118c62415dc95f797a7
Estev0m <estevao.brizante@compasso.com.br>
2021-06-29 14:43:35 -0300

    update index

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ app.get("/repository", async (req, resp) => {
   try {
     const { data } = await axios.get('https://api.github.com/users/takenet/repos')
     const formatDate = data.filter(repo => repo.language == 'C#' 
-)
+    ).sort((a, b) => {
+     return new Date(b.created_at) - new Date(a.created_at)
+      
+})
     return resp.json(formatDate)
   } catch (error) {
     console.log(error)
